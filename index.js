5f6df559933d5470dcff7da3dd7c4febefb94f06
Mandy <mandykparson@gmail.com>
2021-03-25 19:43:41 -0600

    adding js file

---
+++ index.js
@@ -0,0 +1,33 @@
+//const input = document.getElementById('input');
+
+//function randomNumber() {
+    //let randomlyGenNum = Math.floor((Math.random() * 5) + 1);
+    //console.log(randomlyGenNum)
+    //alert(`Your number is ${randomlyGenNum}`)
+//}
+
+//input.addEventListener('click', randomNumber); 
+
+ // the number generated when a use clicks on the Take a Number button
+ const eneteredNumber = "" // the number inputed (may not be takeANumber) by user
+ const prizes = [
+     "a puppy", 
+     "a company mug", 
+     "a discounted christmas sweater from Walmart", 
+     "the CD: Now That's What I Call Music 14", 
+     "Kazaam on DVD/BlueRay", 
+     "a computer virus",
+     "the iMac rainbow wheel",
+ 
+ ]
+ 
+ function selectPrize(){
+     console.log(`Congratulations! You won ${prizes[takeaNumber]}!`)
+ }
+ 
+ 
+ function randomNumber() {
+     let randomlyGenNum = Math.floor((Math.random() * 5) + 1);
+     console.log(randomlyGenNum)
+     alert('I was clicked!')
+ }
